2d0800cfc40696472c6bbfec8cc02aaebc783f8e
Albert Visser <albert@lemoncurry>
2015-03-19 21:08:03 +0100

    bugfixes wrt adding/inserting below/aftre root and copying attributes

--- axe/axe_qt.py
+++ axe/axe_qt.py
@@ -428,7 +428,6 @@ class MainFrame(gui.QMainWindow, AxeMixin):
     def closeEvent(self, event):
         """applicatie afsluiten"""
         test = self.check_tree()
-        print(test, event)
         if test:
             event.accept()
         else:
@@ -577,21 +576,15 @@ class MainFrame(gui.QMainWindow, AxeMixin):
     def paste(self, ev=None, before=True, pastebelow=False):
         if not self.checkselection():
             return
+        if self.item.parent() == self.top and not pastebelow:
+            self._meldinfo("Can't paste before or after the root")
+            return
         data = (str(self.item.text(1)), str(self.item.text(2)))
         if pastebelow and not str(self.item.text(0)).startswith(ELSTART):
             self._meldfout("Can't paste below an attribute")
             return
-        if data == ((self.rt.tag, self.rt.text) or ""):
-            if before:
-                self._meldfout("Can't paste before the root")
-                return
-            else:
-                self._meldinfo("Pasting as first element below root")
-                pastebelow = True
-        ## if self.cut:
-            ## self.enable_pasteitems(False)
         if self.cut_att:
-            item = getshortname(self.cut_att, self.ns_prefixes, self.ns_uris,
+            item = getshortname((self.cut_att, self.ns_prefixes, self.ns_uris),
                 attr=True)
             node = gui.QTreeWidgetItem()
             node.setText(0, item)
@@ -648,6 +641,9 @@ class MainFrame(gui.QMainWindow, AxeMixin):
 
     def insert(self, ev=None, before=True, below=False):
         if not self.checkselection():
+            return
+        if self.item.parent() == self.top and not below:
+            self._meldinfo("Can't insert before or after the root")
             return
         edt = ElementDialog(self, title="New element").exec_()
         if edt == gui.QDialog.Accepted:
@@ -887,15 +883,17 @@ class MainFrame(gui.QMainWindow, AxeMixin):
         return skip
 
     def checkselection(self, message=True):
-        """get the currently seleted item
+        """get the currently selected item
 
         if there is no selection or the file title is selected, display a message
-        (if requested). I think originally it returned False in that case
+        (if requested). Also return False in that case.
+        Should't it return False also if no message is requested?
         """
         sel = True
         self.item = self.tree.currentItem()
         if message and (self.item is None or self.item == self.top):
             self._meldinfo('You need to select an element or attribute first')
+            sel = False
         return sel
 
     def expand(self, ev=None):
