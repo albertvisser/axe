0dcc01d6684c7b304f9a0c383e7dc8088ec0b5e1
Albert Visser <albert@lemoncurry>
2019-01-16 19:24:31 +0100

    minor logging changes

--- axe/axe_base.py
+++ axe/axe_base.py
@@ -16,13 +16,20 @@ TITEL = "Albert's (Simple) XML editor"
 APATH = pathlib.Path(__file__).parent
 axe_iconame = str(APATH / "axe.ico")
 # always log in program directory
-logging.basicConfig(filename=str(APATH.parent / 'logs' / 'axe_qt.log'),
-                    level=logging.DEBUG, format='%(asctime)s %(message)s')
+LOGFILE = APATH.parent / 'logs' / 'axe_qt.log'
+LOGPLEASE = 'DEBUG' in os.environ and os.environ["DEBUG"] != "0"
+if LOGPLEASE:
+    if not LOGFILE.parent.exists():
+        LOGFILE.parent.mkdir()
+    if not LOGFILE.exists():
+        LOGFILE.touch()
+    logging.basicConfig(filename=str(LOGFILE),
+                        level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 
 def log(message):
     """if enabled, write a line to the log"""
-    if 'DEBUG' in os.environ and os.environ["DEBUG"] != "0":
+    if LOGPLEASE:
         logging.info(message)
 
 
